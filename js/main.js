98509256273d6a1901d1444516abdca361dd68e1
Robert Klim <kontakt.robert.klim@gmail.com>
2018-11-19 11:36:33 +0100

    Create Vehicle class and use it to display text

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 let font
+let vehicles = [];
 
 function preload() {
     font = loadFont('assets/RobotoMono-Bold.ttf');
+}
+
+class Vehicle {
+
+    constructor(x, y) {
+        this.pos = createVector(x, y);
+        this.target = createVector(x, y);
+        this.vel = createVector();
+        this.acc = createVector();
+    }
+
+    update() {
+        this.pos.add(this.vel);
+        this.vel.add(this.acc);
+    }
+
+    show() {
+        stroke(255);
+        strokeWeight(4);
+        point(this.pos.x, this.pos.y);
+    }
+
 }
 
 function setup() {
@@ -17,13 +40,20 @@ function setup() {
 
     for (let i = 0; i < textPoints.length; i++) {
         let p = textPoints[i];
-        stroke(255);
-        strokeWeight(4);
-        point(p.x, p.y);
+        let vehicle = new Vehicle(p.x, p.y);
+        vehicles.push(vehicle);
+        // stroke(255);
+        // strokeWeight(4);
+        // point(p.x, p.y);
     }
 
 }
 
 function draw() {
-
+    background(0);
+    for (let i = 0; i < vehicles.length; i++) {
+        let vehicle = vehicles[i];
+        vehicle.update();
+        vehicle.show();
+    }
 }
