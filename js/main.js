17d75436cce0e69871dc4931a150d7ef436e090f
Robert Klim <kontakt.robert.klim@gmail.com>
2018-11-19 09:45:06 +0100

    Draw a text using points (textToPoints function)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
+let font
+
+function preload() {
+    font = loadFont('assets/RobotoMono-Bold.ttf');
+}
+
 function setup() {
+    createCanvas(1200, 500);
+    background(0);
+    textFont(font);
+    // textSize(128);
+    // fill(255);
+    // noStroke();
+    // text('Pie is a lie!', 35, 280);
+
+    let textPoints = font.textToPoints('Pie is a lie!', 35, 280, 128);
+
+    for (let i = 0; i < textPoints.length; i++) {
+        let p = textPoints[i];
+        stroke(255);
+        strokeWeight(4);
+        point(p.x, p.y);
+    }
 
 }
 
 function draw() {
-    
+
 }
