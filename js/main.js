3fdb8979654c4e5681dcd06de2d79586bb0d4ac1
Robert Klim <kontakt.robert.klim@gmail.com>
2018-11-19 13:23:56 +0100

    Transform one text into another every set amout of frames

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let font
 let vehicles = [];
+let frames = 1;
+let targetTextPoints = [];
+let change = false;
 
 function preload() {
     font = loadFont('assets/RobotoMono-Bold.ttf');
@@ -14,10 +17,10 @@ function setup() {
     // noStroke();
     // text('Pie is a lie!', 35, 280);
 
-    let textPoints = font.textToPoints('Pie is a lie!', 35, 280, 128);
+    targetTextPoints = font.textToPoints('Pie is a lie!', 35, 280, 128);
 
-    for (let i = 0; i < textPoints.length; i++) {
-        let p = textPoints[i];
+    for (let i = 0; i < targetTextPoints.length; i++) {
+        let p = targetTextPoints[i];
         let vehicle = new Vehicle(p.x, p.y);
         vehicles.push(vehicle);
         // stroke(255);
@@ -29,10 +32,47 @@ function setup() {
 
 function draw() {
     background(0);
+    
     for (let i = 0; i < vehicles.length; i++) {
         let vehicle = vehicles[i];
         vehicle.behaviors();
         vehicle.update();
         vehicle.show();
     }
+
+    if (frames % 300 === 0) {
+        targetTextPoints = font.textToPoints('!!eil a si eiP', 35, 280, 128);
+        change = true;
+    } else if (frames % 700 === 0) {
+        targetTextPoints = font.textToPoints('Pie is a lie!', 35, 280, 128);
+        change = true;
+    }
+
+    if (change) {
+        if (targetTextPoints.length >= vehicles.length) {
+            for (let i = 0; i < targetTextPoints.length; i++) {
+                if (i < vehicles.length) {
+                    let p = targetTextPoints[i];
+                    vehicles[i].target.set(p.x, p.y);
+                } else {
+                    let p = targetTextPoints[i];
+                    let index = floor(random(vehicles.length));
+                    vehicles[i] = new Vehicle(p.x, p.y);
+                    vehicles[i].pos.set(vehicles[index].pos.x, vehicles[index].pos.y)
+                }
+            }
+        } else {
+            for (let i = 0; i < vehicles.length; i++) {
+                if (i < targetTextPoints.length) {
+                    let p = targetTextPoints[i];
+                    vehicles[i].target.set(p.x, p.y);
+                } else {
+                    vehicles.splice(i, vehicles.length);
+                }
+            }
+        }
+        change = false;
+    }
+
+    frames++;
 }
